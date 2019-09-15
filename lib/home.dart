86b7a7a0401c5ee2cb7f3a988e7cc08ca43a0dbf
Rami ahmed <35673698+ramisadek@users.noreply.github.com>
2019-09-15 09:15:03 +0200

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tiny example"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
