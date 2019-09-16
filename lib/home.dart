51459217e2fa37f40721ced18013bf49b3e506d2
Rami ahmed <35673698+ramisadek@users.noreply.github.com>
2019-09-16 11:55:32 +0200

    firebase auth with google note: the fireauth gives me a problem that he is not a subtype of firebase when i get packege for another one then change it back to any it works !

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,13 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tiny_example/secondScreen.dart';
+
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -9,35 +16,111 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  Timer timer;
-  int time=0;
-  Widget hallo(){
-    time++;
-    setState(() {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  SharedPreferences prefs;
+
+  bool isLoading = false;
+  bool isLoggedIn = false;
+  FirebaseUser currentUser;
+
+  Future<Null> handleSignIn() async {
+    prefs = await SharedPreferences.getInstance();
+
+    this.setState(() {
+      isLoading = true;
     });
-    return Text("$time",style: TextStyle(color: Colors.blue,fontSize: 40),);
-  }
 
-  @override
-  void initState() {
-    super.initState();
-    timer = Timer.periodic(Duration(seconds: 15), (Timer t) => hallo());
-  }
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
 
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    FirebaseUser firebaseUser = await firebaseAuth.signInWithCredential(credential);
+
+    if (firebaseUser != null) {
+      // Check is already sign up
+      final QuerySnapshot result =
+      await Firestore.instance.collection('users').where('id', isEqualTo: firebaseUser.uid).getDocuments();
+      final List<DocumentSnapshot> documents = result.documents;
+      if (documents.length == 0) {
+        // Update data to server if new user
+        Firestore.instance.collection('users').document(firebaseUser.uid).setData({
+          'nickname': firebaseUser.displayName,
+          'photoUrl': firebaseUser.photoUrl,
+          'id': firebaseUser.uid,
+          'createdAt': DateTime.now().millisecondsSinceEpoch.toString(),
+          'chattingWith': null
+        });
+
+        // Write data to local
+        currentUser = firebaseUser;
+        await prefs.setString('id', currentUser.uid);
+        await prefs.setString('nickname', currentUser.displayName);
+        await prefs.setString('photoUrl', currentUser.photoUrl);
+      } else {
+        // Write data to local
+        await prefs.setString('id', documents[0]['id']);
+        await prefs.setString('nickname', documents[0]['nickname']);
+        await prefs.setString('photoUrl', documents[0]['photoUrl']);
+        await prefs.setString('aboutMe', documents[0]['aboutMe']);
+      }
+      Fluttertoast.showToast(msg: "Sign in success");
+      this.setState(() {
+        isLoading = false;
+      });
+      print("*********************************************");
+      print(firebaseUser.uid);
+      print("*********************************************");
+      Navigator.push(context, MaterialPageRoute(builder: (context) => secondScreen( firebaseUser.uid)));
+    } else {
+      Fluttertoast.showToast(msg: "Sign in fail");
+      this.setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Tiny example"),
+        title: Text("Sokar_test"),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            hallo(),
-          ],
-        ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: FlatButton(
+                onPressed: handleSignIn,
+                child: Text(
+                  'SIGN IN WITH GOOGLE',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                color: Color(0xffdd4b39),
+                highlightColor: Color(0xffff7f7f),
+                splashColor: Colors.transparent,
+                textColor: Colors.white,
+                padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0)),
+          ),
+
+          // Loading
+          Positioned(
+            child: isLoading
+                ? Container(
+              child: Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                ),
+              ),
+              color: Colors.white.withOpacity(0.8),
+            )
+                : Container(),
+          ),
+        ],
       ),
     );
   }
