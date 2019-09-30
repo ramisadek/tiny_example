2442604fa12245451c52d32e2e212a5648e2fa0b
Rami ahmed <35673698+ramisadek@users.noreply.github.com>
2019-09-30 16:46:03 +0200

    chat app using firebase

--- lib/login.dart
+++ lib/login.dart
@@ -3,19 +3,33 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:tiny_example/const.dart';
+import 'package:tiny_example/main.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:tiny_example/secondScreen.dart';
 
-
-class MyHomePage extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Demo',
+      home: LoginScreen(title: 'CHAT DEMO'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key key, this.title}) : super(key: key);
 
+  final String title;
+
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
   final GoogleSignIn googleSignIn = GoogleSignIn();
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   SharedPreferences prefs;
@@ -23,6 +37,32 @@ class _MyHomePageState extends State<MyHomePage> {
   bool isLoading = false;
   bool isLoggedIn = false;
   FirebaseUser currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    isSignedIn();
+  }
+
+  void isSignedIn() async {
+    this.setState(() {
+      isLoading = true;
+    });
+
+    prefs = await SharedPreferences.getInstance();
+
+    isLoggedIn = await googleSignIn.isSignedIn();
+    if (isLoggedIn) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => MainScreen(currentUserId: prefs.getString('id'))),
+      );
+    }
+
+    this.setState(() {
+      isLoading = false;
+    });
+  }
 
   Future<Null> handleSignIn() async {
     prefs = await SharedPreferences.getInstance();
@@ -44,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
     if (firebaseUser != null) {
       // Check is already sign up
       final QuerySnapshot result =
-      await Firestore.instance.collection('users').where('id', isEqualTo: firebaseUser.uid).getDocuments();
+          await Firestore.instance.collection('users').where('id', isEqualTo: firebaseUser.uid).getDocuments();
       final List<DocumentSnapshot> documents = result.documents;
       if (documents.length == 0) {
         // Update data to server if new user
@@ -75,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
       print("*********************************************");
       print(firebaseUser.uid);
       print("*********************************************");
-      Navigator.push(context, MaterialPageRoute(builder: (context) => secondScreen( firebaseUser.uid)));
+      Navigator.push(context, MaterialPageRoute(builder: (context) => MainScreen(currentUserId: firebaseUser.uid)));
     } else {
       Fluttertoast.showToast(msg: "Sign in fail");
       this.setState(() {
@@ -87,41 +127,43 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sokar_test"),
-        centerTitle: true,
-      ),
-      body: Stack(
-        children: <Widget>[
-          Center(
-            child: FlatButton(
-                onPressed: handleSignIn,
-                child: Text(
-                  'SIGN IN WITH GOOGLE',
-                  style: TextStyle(fontSize: 16.0),
-                ),
-                color: Color(0xffdd4b39),
-                highlightColor: Color(0xffff7f7f),
-                splashColor: Colors.transparent,
-                textColor: Colors.white,
-                padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0)),
+        appBar: AppBar(
+          title: Text(
+            widget.title,
+            style: TextStyle(color: primaryColor, fontWeight: FontWeight.bold),
           ),
+          centerTitle: true,
+        ),
+        body: Stack(
+          children: <Widget>[
+            Center(
+              child: FlatButton(
+                  onPressed: handleSignIn,
+                  child: Text(
+                    'SIGN IN WITH GOOGLE',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  color: Color(0xffdd4b39),
+                  highlightColor: Color(0xffff7f7f),
+                  splashColor: Colors.transparent,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0)),
+            ),
 
-          // Loading
-          Positioned(
-            child: isLoading
-                ? Container(
-              child: Center(
-                child: CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-                ),
-              ),
-              color: Colors.white.withOpacity(0.8),
-            )
-                : Container(),
-          ),
-        ],
-      ),
-    );
+            // Loading
+            Positioned(
+              child: isLoading
+                  ? Container(
+                      child: Center(
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+                        ),
+                      ),
+                      color: Colors.white.withOpacity(0.8),
+                    )
+                  : Container(),
+            ),
+          ],
+        ));
   }
 }
